f790148026ef19515a9fb173b3dd5fbf162257ae
zbaker <zcbaker@ymail.com>
2015-04-20 22:15:02 -0700

    Finished Screencast

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 // My Scripts
 
-//var year = Date.getYear();
+// Get Name & Store Name
+var name = prompt('What is your name?');
 
-console.log("Hello World!");
+// Write/Display Name
+$('.name').text(name);
+
+// Get Major & Store Major
+var major = prompt('What is your Major?');
+
+// Write/Display Major
+$('.major').text(major);
+
+// Apply Lettering.js to H1
+$('h1').lettering();
+
+// Apply lettering.js to Name
+$('.name').lettering('words');
